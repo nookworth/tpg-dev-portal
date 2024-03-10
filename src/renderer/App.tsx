5fc6c3cb7c28153150af0e1b50528ef15c1014ad
nookoid <christopher.morrison@travelpassgroup.com>
2024-03-09 19:19:56 -0700

    Initial commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function MainPage() {
+  return <div />;
+}
+
+export default function App() {
+  return MainPage();
+}
